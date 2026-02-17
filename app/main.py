8e698f7b17649b6b21f8cf82f049abdccf0c119f
Anirudh Sathish <anirudhsat8@gmail.com>
2026-02-17 14:44:00 -0500

    Docker files added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.api.routes import analysis, metrics
 from app.core.config import settings
+from app.db.base import Base
+from app.db.session import engine
+import app.db.models
+from contextlib import asynccontextmanager
 
 app = FastAPI(title=settings.app_name)
 
@@ -10,3 +14,17 @@ app.include_router(metrics.router)
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # ---- startup logic ----
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+    # shutdown logic here 
+
+
+
+
+app = FastAPI(lifespan=lifespan)
